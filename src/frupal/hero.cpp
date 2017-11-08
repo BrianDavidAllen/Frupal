a930f380347e4f8cf72588d6406a47320b9ab5f9
Paul_Hubbard.prsn <phubbard67@gmail.com>
2017-11-07 17:04:10 -0800

    Add files via upload
    Added update hero and check hero health functions

--- src/frupal/hero.cpp
+++ src/frupal/hero.cpp
@@ -13,16 +13,16 @@
 using namespace std;
 
 //Default constructor: sets energy level to 100, x,y to zero, whiffles to zero, and name/invetory to NULL
-Hero::Hero() : energy(100), whiffles(0), x(0), y(0), name(""), inventory("")
+Hero::Hero() : energy(100), whiffles(0), x(0), y(0), name(""), inventory(""), alive(true)
 {
     
 }
 
 
-//Parameterized constructor: sets the name to a string passed in, everything else is set to default values
-Hero::Hero(int energy, int whiffles, int x, int y, string newName, string inventory) : energy(100), whiffles(0), x(0), y(0), name(newName), inventory("")
+//Parameterized constructor: sets energy/whiffles/name/inventory to the passed in arguments
+Hero::Hero(int energy, int whiffles, int x, int y, string name, string inventory) : energy(energy), whiffles(whiffles), x(0), y(0), name(name), inventory(inventory)
 {
-
+    setCoords(x,y);
 }
 
 //Hero destructor
@@ -37,8 +37,8 @@ void Hero::displayHero()
     cout << "Name: " << name << endl;
     cout << "Energy level: " << energy << endl;
     cout << "Whiffle count: " << whiffles << endl;
-    cout << "Inventory contents: " << inventory << endl;
-    cout << "X,Y coordinates: " << x << "," << y << endl << endl;
+    cout << "Inventory contents : " << inventory << endl;
+    cout << "X,Y coordinates : " << x << "," << y << endl << endl;
 }
 
 //Sets the Hero's name to a string passed in
@@ -54,17 +54,10 @@ string Hero::getName()
 }
 
 
-//Sets the Hero's whiffles by adding to the hero's whiffles
-//Pass in a negative value to subtract from the hero's whiffles
-void Hero::changeWhiffles(int newWhiffles)
-{
-    whiffles += newWhiffles;
-}
-
-//Sets the whiffles to a value passed in
+//Sets the Hero's whiffles to a value passed in, adds it to the hero's whiffles.
 void Hero::setWhiffles(int newWhiffles)
 {
-    whiffles = newWhiffles;
+    whiffles += newWhiffles;
 }
 
 //Rerturns the hero's whiffle count
@@ -89,29 +82,10 @@ string Hero::getInventory()
     return inventory;
 }
 
-//Change the hero's energy, pass in a negative value to subtract, positive to add
-void Hero::changeEnergy(int newEnergy)
-{
-
-    if(energy + newEnergy > 100)
-    {
-        energy = 100;
-        return;
-    }
-    if(energy + newEnergy < 0)
-    {
-        energy = 0;
-        return;
-    }
-
-    energy += newEnergy; 
-
-}
-
-//sets the energy to a value passed in
+//Sets the hero's energy, pass in a negative value to subtract
 void Hero::setEnergy(int newEnergy)
 {
-    energy = newEnergy;
+    energy += newEnergy; 
 }
 
 //Returns the hero's energy
@@ -140,18 +114,122 @@ int Hero::getY()
 }
 
 
-//toJson function
-json Hero::toJson()
+
+
+
+//Paul Hubbard's funky functions
+
+//Function that takes the value
+//of the player's x and y coordinates
+//and updates the hero's state file
+void Hero::updateHero(int newX, int newY, int addedWhiffles)
 {
-   json output;
+	int startPosX = 0;
+	int startPosY = 0;
+	string fileToOpen = "heroState.txt";
+	string xcord, ycord, whiffles, energy;
+	ifstream stateFile(fileToOpen.c_str());
 
-   output["x"] = x;
-   output["y"] = y;
-   output["energy"] = energy;
-   output["name"] = name;
-   output["inventory"] = inventory;
+	//read in from the file
+	getline(stateFile, energy);
+	getline(stateFile, whiffles);
+	getline(stateFile, xcord);
+	getline(stateFile, ycord);
 
-   return output;
+	
+	//set the player's 
+	//values from last turn
+	this->energy = atoi(energy.c_str());
+	this->whiffles = atoi(whiffles.c_str());
+	this->x = atoi(xcord.c_str());
+	this->y = atoi(ycord.c_str());
+
+	//compare the player's prev x and y
+	//by checking if x-x or y-y equals 0
+	//if so, lower the player's energy by 1
+
+	int checkx = (this->x - newX);
+	int checky = (this->y - newY);
+
+	if(checkx != 0 || checky != 0)
+	{
+		--this->energy;
+	}
+
+	this->x = newX;
+	this->y = newY;
+
+	this->whiffles += addedWhiffles;
+
+	//checks for negative 
+	//whiffle count	
+	if(this->whiffles <= 0)
+	{
+		this->whiffles = 0;
+	}
+
+	//checks if the player is dead
+	//and if they are the file 
+	if(this->energy <= 0)
+	{
+		this->energy = 100;
+		this->whiffles = 0;
+		this->x = startPosX;
+		this->y = startPosY;
+		this->alive = false;
+	}
+
+	//delete file
+	remove(fileToOpen.c_str());
+	
+	//create the new state file
+	ofstream newStateFile;
+	newStateFile.open(fileToOpen.c_str());
+
+	//add the new player info to 
+	//the file	
+	newStateFile << this->energy << '\n'
+		     << this->whiffles << '\n'
+		     << this->x << '\n'
+		     << this->y << '\n';
+	//close and save the file
+	newStateFile.close();
+	
 }
+
+//returns true if the hero's energy level
+//is above 0, and false if the hero is dead
+bool Hero::energyStatus()
+{
+	return this->alive;
+}
+	
+//Paul Hubbard's funky functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
