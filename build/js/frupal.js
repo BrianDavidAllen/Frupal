7c1969a3af45f75011043a04cc9a364511c8a732
Brian Allen <briallen@emperor.cs.pdx.edu>
2017-11-24 14:36:48 -0800

    added buttons that will apear when on an obstacle showing the appropriate tools. Buttons have no functionality yet

--- build/js/frupal.js
+++ build/js/frupal.js
@@ -9,6 +9,7 @@ var mapDimensions = 25;
 var jsonVisible = false;
 var commandInProgress = false;
 var inputDelay = 200; //in ms
+var onObstacle = false; 
 
 var enableDebugMode = function() {
     var elements = document.getElementsByClassName("false");
@@ -34,18 +35,39 @@ function Game() {
         var code = event.keyCode;
         var command;
         console.log("Key pressed");
-        if(code == 37) //Left
-            command = "left";
-        else if(code == 38) //Up
-            command = "up";
-        else if(code == 39) //Right
-            command = "right";
-        else if(code == 40) //Down
-            command = "down";
-        else if(code == 32) //Spacebar
-            command = "space";
-        else
-            return;  //Ignore if some other key is pressed
+	onObstacle = data.obstacle;
+	if(onObstacle == "true"){
+		document.getElementById('toolButtons').style.display = 'block'; 
+		document.getElementById("tool1").innerText = data.tool1;
+		document.getElementById("tool2").innerText = data.tool2;
+		document.getElementById("tool3").innerText = data.tool3;
+		document.getElementById("noTool").innerText = data.noTool;
+		if(code == 49) 
+			command = "tool1";
+		else if(code == 50)
+			command = "tool2";
+		else if(code == 51)
+			command = "tool3";
+		else if(code == 52)
+			command = "noTool";
+		else
+			return;
+	}
+	else{
+		document.getElementById("toolButtons").style.display = "none";
+		if(code == 37) //Left
+            		command = "left";
+        	else if(code == 38) //Up
+           		command = "up";
+        	else if(code == 39) //Right
+            		command = "right";
+        	else if(code == 40) //Down
+            		command = "down";
+        	else if(code == 32) //Spacebar
+           		command = "space";
+        	else
+            	return;  //Ignore if some other key is pressed
+	}
         
         this.sendCommand(command);
     }
